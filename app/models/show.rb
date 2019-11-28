fde8d0af07805c557e8c381d01ecc343418bf4b3
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-11-28 12:06:39 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,25 +1,25 @@
 class Show < ActiveRecord::Base
 
   def highest_rating
-    Show.maximum("rating")
+    Show.maximum(:rating)
   end
 
   def most_popular_show
-    show = Show.maximum("rating")
+    show = Show.maximum(:rating)
     show.name
   end
 
   def lowest_rating
-    Show.minimum("rating")
+    Show.minimum(:rating)
   end
 
   def least_popular_show
-    show = Show.minimum("rating")
+    show = Show.minimum(:rating)
     show.name
   end
 
   def rating_sums
-    Show.sum("rating")
+    Show.sum(:rating)
   end
 
   def popular_shows
@@ -27,5 +27,5 @@ class Show < ActiveRecord::Base
   end
 
   def shows_by_alphabetical_order
-    Show.order("name") :ASC
+    Show.order(:name) :ASC
   end
