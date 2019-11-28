96312a8bf341231c0791d169d03c09f274cab04d
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-11-28 11:59:44 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,7 +6,25 @@ class Show < ActiveRecord::Base
 
   def most_popular_show
     show = Show.maximum("rating")
-    show.title
+    show.name
   end
 
-  def
+  def lowest_rating
+    Show.minimum("rating")
+  end
+
+  def least_popular_show
+    show = Show.minimum("rating")
+    show.name
+  end
+
+  def rating_sums
+    Show.sum("rating")
+  end
+
+  def popular_shows
+    Show.where("rating => ?", 5[:names])
+  end
+
+  def 
+    
